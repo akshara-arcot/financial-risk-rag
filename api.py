91ab1fc5a4a50d2f7d8223a38e24110e63a29d41
akshara-arcot <akshuarcot@gmail.com>
2026-04-02 16:28:46 -0500

    Implement FastAPI for Financial Risk RAG API

---
+++ api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rag_pipelie import ask_query
+
+app = FastAPI(title = "Financial Risk RAG API")
+
+class QueryRequest(BaseModel):
+  query:str
+
+@app.get("/")
+def health_check():
+  return {"status":"ok"}
+
+@app.post("/query")
+def query_rag(request:QueryRequest):
+  answer = ask_query(request.query)
+  return {"query" : request.query, "answer" : answer} 
